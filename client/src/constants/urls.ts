285c8814d10b53e1ba7c631a5a24f062a57b09c2
IdentityZero <lovelycadiente1@gmail.com>
2025-04-05 11:31:59 +0800

    Updated URLs

--- client/src/constants/urls.ts
+++ client/src/constants/urls.ts
@@ -1,15 +1,20 @@
-export const BASE_API_URL = "localhost:8000";
+const BASE_API_URL_VALUE = import.meta.env.VITE_API_BASE_URL;
+
+const protocol = BASE_API_URL_VALUE ? "ws://" : "wss://";
+const httpProtocol = BASE_API_URL_VALUE ? "http://" : "";
+
+export const BASE_API_URL = BASE_API_URL_VALUE || window.location.host;
 
 export const LIVE_ANNOUNCEMENT_URL =
-  "ws://" + BASE_API_URL + "/ws/live-announcement/";
+  protocol + BASE_API_URL + "/ws/live-announcement/";
 
 export const LIVE_CONTENT_UPDATE_URL =
-  "ws://" + BASE_API_URL + "/ws/live-content-updates/";
+  protocol + BASE_API_URL + "/ws/live-content-updates/";
 
-export const SITE_SETTINGS_URL = "http://" + BASE_API_URL + "/settings/";
+export const SITE_SETTINGS_URL = httpProtocol + BASE_API_URL + "/settings/";
 
-export const FIELD_DEVICES_URL = "ws://" + BASE_API_URL + "/ws/field-devices/";
+export const FIELD_DEVICES_URL = protocol + BASE_API_URL + "/ws/field-devices/";
 
 export const NOTIFICATIONS_URL = (id: string) => {
-  return "ws://" + BASE_API_URL + "/ws/notifications/" + id + "/";
+  return protocol + BASE_API_URL + "/ws/notifications/" + id + "/";
 };
