e0f97e455ca0106a87fbb3b0937986a6c3f06b3e
IdentityZero <lovelycadiente1@gmail.com>
2025-05-14 22:34:33 +0800

    Removed CO2 in coe conditions and enlarged texts

--- client/src/features/KioskDisplay/Footer/CoeConditions.tsx
+++ client/src/features/KioskDisplay/Footer/CoeConditions.tsx
@@ -21,21 +21,25 @@ const getStatusColor = (type: SensorType, value: number): string => {
   }
 };
 
-const CoeConditions = ({ temperature, humidity, co2 }: SensorDataType) => {
+const CoeConditions = ({
+  temperature,
+  humidity,
+  co2: _co2,
+}: SensorDataType) => {
   return (
     <div className="relative flex flex-col items-center rounded-2xl shadow-lg h-full w-full px-1">
       <div className="absolute inset-0 bg-gradient-to-br from-blue-500/40 to-purple-500/30 rounded-2xl blur-2xl"></div>
       <div className="relative z-10 text-3xl font-semibold">COE Conditions</div>
 
       {/* {isLoading && <LoadingMessage message="Loading..." />} */}
-      <div className="relative z-10 p-4 rounded-2xl shadow-lg space-y-4">
+      <div className="relative z-10 p-4 rounded-2xl shadow-lg space-y-4 h-full flex flex-col items-center justify-evenly">
         <Card
           label="Temperature"
           icon="🌡️"
           value={temperature}
           type="temperature"
         />
-        <Card label="CO2 Levels" icon="🫁" value={co2} type="co2" />
+        {/* <Card label="CO2 Levels" icon="🫁" value={co2} type="co2" /> */}
         <Card label="Humidity" icon="💧" value={humidity} type="humidity" />
       </div>
     </div>
@@ -58,10 +62,10 @@ function Card({ label, value, icon, type }: CardProps) {
 
   return (
     <div className="rounded-xl flex flex-col items-center justify-between text-white">
-      <div className="font-semibold text-xl flex">
+      <div className="font-semibold text-3xl flex">
         {icon} {label}
       </div>
-      <div className={`text-3xl font-bold ${getStatusColor(type, value)}`}>
+      <div className={`text-5xl font-bold ${getStatusColor(type, value)}`}>
         {value}
         {unitMap[type]}
       </div>
